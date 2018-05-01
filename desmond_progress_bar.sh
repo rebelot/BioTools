0b3b5351a280e08dec8e88c9b5bc7033c9f46afa
tommasolaurenzi <tommaso.laurenzi@gmail.com>
2018-05-01 18:11:35 +0200

    added desmond_progress_bar.sh

--- desmond_progress_bar.sh
+++ desmond_progress_bar.sh
@@ -1,18 +1,18 @@
 LOGFILE=$1
-TIME=$2
+TIME=$(grep -m 1 ' time = "[[:alnum:]]*\.[[:alnum:]]' $LOGFILE | awk '{print $3}' | sed 's/"//g')
+TIME=$(python -c "print($TIME/1000)")
 
 echo
 
 while [[ "$(tail -n 1 $LOGFILE | awk '{ print $1 }')" =~ "Chemical" ]]; do
     ct="$(tail -n 1 $LOGFILE | awk '{ print $3 }')"
     v="$(tail -n 1 $LOGFILE | awk '{ print $8 }')"
-    perh=$(python -c "print($v/24.0)")
-    comp=$(python -c "print(100*$ct/$TIME)")
-    diff=$(python -c "print($TIME - $ct)")
-    seta=$(python -c "print(round(3600*($diff/1000.)/$perh))")
-    eta=$(printf '%dh:%dm:%ds\n' $(($seta/3600)) $(($seta%3600/60)) $(($seta%60)))
+    ctns=$(python -c "print($ct/1000)")
+    comp=$(python -c "print(100*$ctns/$TIME)")
+    sec_left=$(python -c "print(round(24*3600 * ($TIME - $ctns)/$v ))")
+    eta=$(printf '%0.2dd:%0.2dh:%0.2dm:%0.2ds\n' $(($sec_left/86400)) $(($sec_left/3600%24)) $(($sec_left%3600/60)) $(($sec_left%60)))
     
-    printf "\e[0K\rCompletion: %.2f%%       ETA: %11s" $comp $eta
+    printf "\e[0K\rCompletion: %.2f of $TIME ns (%.2f%%)  @ $v ns/day     ETA: %15s" $ctns $comp $eta
     sleep 1
 done
 
