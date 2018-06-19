a972e3695bbac2c2188004a5ca1e22eda78b3417
tommasolaurenzi <tommaso.laurenzi@gmail.com>
2018-06-19 14:37:29 +0200

    update

--- pdbrenumber.py
+++ pdbrenumber.py
@@ -1,55 +1,61 @@
 #!/usr/bin/env python
 
 import sys
-import os
+import argparse
+
 
 def main():
-    try:                                                # check if there are arguments
-        pdbin_name = sys.argv[1]
-        if pdbin_name in ("-h", "--help", "-help"):
-            print("pdbrenumber source.pdb [out.pdb]")
-            return 0
-    except:                                             # if no arguments, return usage
-        print("pdbrenumber source.pdb [out.pdb]")
-        return 1
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pdb", help="pdb input file")
+    parser.add_argument("-b", "--base-number", type=int, default=0,
+                        help="offset base number from which renumbering starts")
+    parser.add_argument("-t", "--res-type",
+                        choices=["atom", "het"], default="atom",
+                        help="choose which residues to renumber")
+    parser.add_argument("-o1k", "--over-1k", action="store_true", help="if checked, \
+                        residuse > 1000 will not be renumbered from 1")
 
-    pdbin = open(pdbin_name, 'r')                       # open pdb for reading
+    args = parser.parse_args()
 
-    try:                                                # check for output name in arguments
-        pdbout_name = sys.argv[2]
-        try:                                                # try to create the given filename
-            pdbout = open(pdbout_name, 'x')
-        except FileExistsError:                             # if file already exists, erase it and replace it
-            os.remove(pdbout_name)
-            pdbout = open(pdbout_name, 'x')
+    pdbin = args.pdb
+    basenum = args.base_number
+    restype = args.res_type
+    o1k = args.over_1k
 
-    except IndexError:                                  # if no output filename is given, write to stdout
-        pdbout = sys.stdout
+    pdbin = open(pdbin, 'r')
 
-    newresnum = 0
+    if restype == "atom":
+        restype = "ATOM"
+    elif restype == "het":
+        restype = "HETA"
+    elif restype == "all":
+        restype = ("ATOM", "HETA")
+
+    newresnum = basenum
     oldresnum = 0
 
     firsresidue = True
     for line in pdbin.readlines():
-        if 'ATOM' == line[0:4]:                         # select ATOM coordinates lines
-            resnum = line[22:26]
+        if line[0:4] in restype:
+            resnum = int(line[22:26])
 
-            if firsresidue:                             # executed only for the first loop cycle
-                oldresnum = resnum                      # initialize oldresnum with the first residue number
+            if firsresidue:
+                oldresnum = resnum
                 firsresidue = False
 
-            if resnum != oldresnum:                     # if the current residue number is different from the previous
-                newresnum += 1                          # then a new residue is encountered and oldresnum is updated
+            if resnum != oldresnum:
+                newresnum += 1
                 oldresnum = resnum
 
-            if newresnum > 9999:                        # if more than 10000 residues are encountered, start renumbering
-                print("WARNING: residues > 10000")
-                newresnum = 0
+            if newresnum > 9999:
+                sys.stderr.write("WARNING: residues > 10000")
+                newresnum = resnum if o1k else 0
 
-            pdbout.write(line[:22] + "{:>4}".format(newresnum) + line[26:])
+            sys.stdout.write(line[:22] + "{:>4}".format(newresnum) + line[26:])
 
         else:
-            pdbout.write(line)
+            sys.stdout.write(line)
+
 
 if __name__ == '__main__':
     main()
