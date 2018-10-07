704b2bf2f300e30a0ed5092a077b8d700184243c
rebelot <tommaso.laurenzi@gmail.com>
2018-10-07 14:43:01 +0200

    added output basename argument

--- schrodinger_scripts/trj_extralc.sh
+++ schrodinger_scripts/trj_extralc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "-h" ]; then
-    echo usage: extralc IN.cms IN_trj extract_asl align_asl center_asl
+    echo usage: extralc IN.cms IN_trj extract_asl align_asl center_asl output_name
     exit
 fi
 
@@ -10,30 +10,30 @@ in_trj=$2
 extr_asl=$3
 al_asl=$4
 c_asl=$5
+output_name=$6
 
 echo "extracting \"$extr_asl\" subsystem from ${in_cms%.*}..."
-$SCHRODINGER/run trj_extract_subsystem.py "$in_cms" extracted -t "$in_trj" -asl "$extr_asl"
+$SCHRODINGER/run trj_extract_subsystem.py "$in_cms" $output_name\_extracted -t "$in_trj" -asl "$extr_asl"
 echo "extracted-out.cms extracted_trj written"
 echo
 echo "aligning \"$al_asl\" subsystem..."
-$SCHRODINGER/run trj_align.py extracted-out.cms extracted_trj aligned -asl "$al_asl"
+$SCHRODINGER/run trj_align.py $output_name\_extracted-out.cms $output_name\_extracted_trj $output_name\_aligned -asl "$al_asl"
 echo "aligned-out.cms aligned_trj written"
 echo   
 echo "centering \"$c_asl\" subsystem..."
-$SCHRODINGER/run trj_center.py aligned-out.cms extralc -t aligned_trj -asl "$c_asl"
+$SCHRODINGER/run trj_center.py $output_name\_aligned-out.cms $output_name\_extralc -t $output_name\_aligned_trj -asl "$c_asl"
 echo "extralc-out.cms extralc_trj written"
 echo
 
 echo "removing
-$(find . -name 'extracted*')"
+$(find . -name $output_name\_extracted*)"
 echo "proceed? y/n"
 read input
-[[ $input = "y" ]] && rm -rf extracted* 
+[[ $input = "y" ]] && rm -rf $output_name\_extracted* 
 
 echo
 echo "removing
-$(find . -name 'aligned*')"
+$(find . -name $output_name\_aligned*)"
 echo "proceed? y/n"
 read input
-[[ $input = "y" ]] && rm -rf aligned* 
-
+[[ $input = "y" ]] && rm -rf $output_name\_aligned*
