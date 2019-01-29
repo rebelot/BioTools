e9bbb3f5c19ec9f6ffe316f026cae7e298d64dd3
rebelot <tommaso.laurenzi@gmail.com>
2019-01-29 13:13:05 +0100

    fixed wrong numpy slicing; some consistency of command line arguments

--- schrodinger_scripts/trj_fcluster.py
+++ schrodinger_scripts/trj_fcluster.py
@@ -1,9 +1,12 @@
+#!/opt/schrodinger/suites2018-4/run
+
 from schrodinger.application.desmond.packages import topo, traj, traj_util, analysis
 from schrodinger.structure import StructureWriter
 from scipy.spatial.distance import squareform
 from scipy.cluster.hierarchy import linkage, fcluster, dendrogram
 import numpy as np
 from scipy.spatial.distance import pdist
+import matplotlib.pyplot as plt
 import argparse
 import sys
 
@@ -11,11 +14,10 @@ def main():
     args = argparse.ArgumentParser(description="Perform RMSD-based clustering of a Desmond MD")
     args.add_argument('cms', help="Cms input file")
     args.add_argument('out', help="Output base name")
-    args.add_argument('rmsd', help="Define ASL for RMSD calculation")
-    args.add_argument('-fit', help="Define ASL for fitting")
-    args.add_argument('-n', help="Max number N of clusters")
+    args.add_argument('-rmsd', help="Define ASL for RMSD calculation", required=True)
+    args.add_argument('-fit', help="Define ASL for fitting;", default=False)
+    args.add_argument('-n', help="Max number N of clusters", required=True)
     args.add_argument('-s', help="Split trajectory in sub trajectories containing frames of each cluster", action="store_true")
-    args.add_argument('-pre', help="Speed up calculation if using prealigned (fitted) traj, or if fitting is not required", action="store_true")
     args.add_argument('-tree', help="View cluster dendrogram and exit", action="store_true")
     args.add_argument('-save', help="Store intermediate distance matrix to FILE", default=False)
     args.add_argument('-restore', help="Load intermediate distance matrix from FILE", default=False)
@@ -33,20 +35,6 @@ def main():
         rmsd_matrix = np.fromfile(args.restore)
         rmsd_matrix.reshape(int(np.sqrt(len(rmsd_matrix))), int(np.sqrt(len(rmsd_matrix))))
 
-    elif args.pre:
-        f = len(trj)
-        rmsd_matrix = np.empty((f,f))
-        pos = np.array([fr.pos(rmsd_gids) for fr in trj])
-        tot = f * (f - 1) / 2
-        c = 0
-        for i in range(f):
-            ref = pos[i]
-            for j in range(i):
-                rmsd_matrix[i,j] = np.sqrt(np.sum(pdist(ref - pos[j])))
-                rmsd_matrix[j,i] = rmsd_matrix[i,j]
-                c += 1
-                sys.stderr.write(f"\r{c}/{tot} ({c/tot:.5%})")
-
     else:
         fit_aids = cms.select_atom(args.fit)
         fit_gids = topo.aids2gids(cms, fit_aids, include_pseudoatoms=False)
@@ -62,6 +50,7 @@ def main():
     
     if args.tree:
         dendrogram(Z)
+        plt.show()
         return
 
     clusters = []
@@ -75,14 +64,19 @@ def main():
             clusters.append(elements)
             # Get the centroids by individuating the element with the minimum distance to
             # every other element of the same cluster
-            c = np.argmin(np.sum(rmsd_matrix[elements, :][elements], axis=1))
+            c = np.argmin(np.sum(rmsd_matrix[elements, :][:, elements], axis=1))
             centroids.append(elements[c])
 
-    for i,elements in enumerate(clusters):
+    trj = np.array(trj)
+    for i,elements in enumerate(clusters, 1):
         traj.write_traj(trj[elements], f"{args.out}_{i}_trj")
+	
+    system_st = cms.extract(cms.atom)
 
-    for i,centroid in enumerate(centroids):
-        cms.setXYZ(trj[centroid].pos()).write("f{args.out}_{i}-out.mae")
+    for i,centroid in enumerate(centroids, 1):
+        st = system_st.copy()
+        st.setXYZ(trj[centroid].pos())
+        st.write(f"{args.out}_{i}-out.mae")
         
 
 if __name__ == "__main__":
