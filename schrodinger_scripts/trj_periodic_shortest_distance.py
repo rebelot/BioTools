4d0815596f6201f73a69e908a98019059ca12b3d
rebelot <tommaso.laurenzi@gmail.com>
2019-02-03 15:14:55 +0100

    fixed argument parsing

--- schrodinger_scripts/trj_periodic_shortest_distance.py
+++ schrodinger_scripts/trj_periodic_shortest_distance.py
@@ -1,7 +1,6 @@
 #!/opt/schrodinger/suites2018-4/run
 
 from schrodinger.application.desmond.packages import traj_util, topo
-from scipy.spatial import distance_matrix
 from schrodinger.structutils.measure import get_shortest_distance
 from itertools import product
 import numpy as np
@@ -11,17 +10,18 @@ import argparse
 
 
 def main():
-    args = argparse.ArgumentParser(
-        description='Compute the shortest distance between two sets of atoms across orthorombic PBC')
-    args.add_argument('cms', help='cms input file')
-    args.add_argument('-g1', help='Define first group of atoms',
+    parser = argparse.ArgumentParser(
+        description='Compute the shortest distance between two sets of atoms across orthorombic PBC') # type: argparse.Namespace 
+    parser.add_argument('cms', help='cms input file')
+    parser.add_argument('-g1', help='Define first group of atoms',
                       metavar='ASL', required=True)
-    args.add_argument( '-g2',
+    parser.add_argument( '-g2',
                       help='Define second gruoup of atoms', metavar='ASL')
-    args.add_argument('-o', help='Save results in file', metavar='FILE')
-    args.add_argument('-p', help='Create plot')
-    args.add_argument('-noself', help='Do not compute distances within the same cell. This is useful to calculate the distance between a molecule and its periodic images')
-    args.parse_args()
+    parser.add_argument('-o', help='Save results in file', metavar='FILE')
+    parser.add_argument('-p', help='Create plot', action="store_true")
+    parser.add_argument('-noself', help='Do not compute distances within the same cell. This is useful to calculate the distance between a molecule and its periodic images',
+                      action='store_true')
+    args = parser.parse_args()
 
     msys, cms, trj = traj_util.read_cms_and_traj(args.cms)
 
@@ -35,8 +35,8 @@ def main():
     g2_st = cms.extract(g2_aids)
     g2_atoms = list(g2_st.atom)
 
-    i = (-1, 1, 0)
-    c = np.array(list(product(i, i, i)))
+    i = (0, 1, -1) # --> (0, 0, 0), (0, 0, 1) ...
+    c = np.array(list(product(i, i, i))) if not args.noself else np.array(list(product(i, i, i)))[1:]
 
     def get_periodic_images(P, c, box):
         for c in c * box:
@@ -59,7 +59,7 @@ def main():
     with open(out) as fh:
         for d in dist:
             fh.write(
-                f'{d[0]} {g1_atoms[d[1]].pdbres}{g1_atoms[d[1]].resnum} {g2_atoms[d[2]].pdbres}{g2_atoms[d[2]].resnum}\n')
+                f'{d[0]} {g1_atoms[d[1]].pdbres}{g1_atoms[d[1]].resnum} ({g1_atoms[d[1]].index}) {g2_atoms[d[2]].pdbres}{g2_atoms[d[2]].resnum} ({g1_atoms[d[1]].index})\n')
 
     if args.p:
         o = args.o if args.o else 'trj_shortes_periodic_distance'
