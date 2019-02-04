8f29b84d065fc0cdfd11a6f2ac4bb50d0c12969e
rebelot <tommaso.laurenzi@gmail.com>
2019-02-04 13:47:43 +0100

    fixed bugs, added function to explicitly consider PBCs

--- schrodinger_scripts/trj_periodic_shortest_distance.py
+++ schrodinger_scripts/trj_periodic_shortest_distance.py
@@ -16,12 +16,22 @@ def main():
     parser.add_argument('-g1', help='Define first group of atoms',
                       metavar='ASL', required=True)
     parser.add_argument( '-g2',
-                      help='Define second gruoup of atoms', metavar='ASL')
+                      help='Define second gruoup of atoms', metavar='ASL', required=True)
+    parser.add_argument('-periodic', help='Enable PBCs', action='store_true')
     parser.add_argument('-o', help='Save results in file', metavar='FILE')
     parser.add_argument('-p', help='Create plot', action="store_true")
+    parser.add_argument('-s', help='Skip trajectory, format is "START:END:STEP"')
     parser.add_argument('-noself', help='Do not compute distances within the same cell. This is useful to calculate the distance between a molecule and its periodic images',
                       action='store_true')
     args = parser.parse_args()
+
+    def parseskip(x):
+        if x == '':
+            return None
+        else:
+            return int(x)
+
+    skip = [parseskip(x) for x in args.s.split(':')] if args.s else [0, None, None]
 
     msys, cms, trj = traj_util.read_cms_and_traj(args.cms)
 
@@ -42,15 +52,23 @@ def main():
         for c in c * box:
             yield P + c
 
+
     dist = []
     n = len(trj)
-    for fr in trj:
+    for fr in trj[skip[0]:skip[1]:skip[2]]:
         g1_st.setXYZ(fr.pos(g1_gids))
-        dist_imgs = []
-        for g2_image in get_periodic_images(fr.pos(g2_gids), fr.box.diagonal(), c):
-            g2_st.setXYZ(g2_image)
-            dist_imgs.append(get_shortest_distance(g1_st, st2=g2_st))
-        dist.append(min(dist_imgs))
+
+        if args.periodic:
+            dist_imgs = []
+            for g2_image in get_periodic_images(fr.pos(g2_gids), fr.box.diagonal(), c):
+                g2_st.setXYZ(g2_image)
+                dist_imgs.append(get_shortest_distance(g1_st, st2=g2_st))
+            dist.append(min(dist_imgs))
+
+        else:
+            g2_st.setXYZ(fr.pos(g2_gids))
+            dist.append(get_shortest_distance(g1_st, st2=g2_st))
+
         sys.stdout.write(f'\rFrame {fr.orig_index} of {n}')
 
     dist = np.array(dist)
@@ -58,12 +76,12 @@ def main():
     out = sys.stdout if not args.o else open(args.o + '.dat', 'x')
     for d in dist:
         out.write(
-            f'{d[0]} {g1_atoms[d[1]].pdbres}{g1_atoms[d[1]].resnum} ({g1_atoms[d[1]].index}) {g2_atoms[d[2]].pdbres}{g2_atoms[d[2]].resnum} ({g1_atoms[d[1]].index})\n')
+            f'{d[0]} {g1_atoms[int(d[1])].pdbres}{g1_atoms[int(d[1])].resnum} ({g1_atoms[int(d[1])].index}) {g2_atoms[int(d[2])].pdbres}{g2_atoms[int(d[2])].resnum} ({g1_atoms[int(d[1])].index})\n')
     out.close()
 
     if args.p:
         o = args.o if args.o else 'trj_shortes_periodic_distance'
-        plt.plot([fr.time for fr in trj], dist[:, 0])
+        plt.plot([fr.time for fr in trj[skip[0]:skip[1]:skip[2]]], dist[:, 0])
         plt.xlabel('time (ps)')
         plt.ylabel('distance (Å)')
         plt.savefig(o + '.png')
