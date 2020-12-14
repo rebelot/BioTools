7c3b902d35025c386080586207f191aae9795f47
rebelot <tommaso.laurenzi@gmail.com>
2020-12-14 14:38:07 +0100

    fixed

--- schrodinger_scripts/trj_rmsd.py
+++ schrodinger_scripts/trj_rmsd.py
@@ -7,21 +7,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+
 def main():
     parser = argparse.ArgumentParser(description="Calculate RMSD of selected atoms over the course of MD simulation.")
     parser.add_argument('cms', help="Input cms file.")
-    parser.add_argument('trj', help="Input trajectory dir")
+    parser.add_argument('-trj', help="Input trajectory dir", default=None)
     parser.add_argument('-mode', help="RMSD or RMSF, default RMSD", default='RMSD')
-    parser.add_argument('-s', help='Slice trajectory [START:END:STEP]')
+    parser.add_argument('-s', help='Slice trajectory START:END:STEP')
     parser.add_argument('-rmsd', help="Atom selection for RMSD calculation, default 'a.pt CA'", default='a.pt CA')
-    parser.add_argument('-fit', help="Atom selection for superposition, defaults to 'rmsd' selection")
-    parser.add_argument('-ref', help="Reference frame (default 0) OR structure filename (not implemented yet)")
-    parser.add_argument('-o', help="Output filename.")
+    parser.add_argument('-fit', help="Atom selection for superposition, defaults to 'rmsd' selection", default=None)
+    parser.add_argument('-ref', help="Reference frame (default 0) OR structure filename (not implemented yet)", default=0, type=int)
+    parser.add_argument('-o', help="Output filename, defaults to stdout.")
     parser.add_argument('-p', help="Plot results", action='store_true')
     args = parser.parse_args()
 
-    msys, cms = topo.read_cms(args.cms)
-    trj = traj.read_traj(args.trj)
+    if not args.trj:
+        msys, cms, trj = traj_util.read_cms_and_traj(args.cms)
+    else:
+        msys, cms = topo.read_cms(args.cms)
+        trj = traj.read_traj(args.trj)
 
     if args.s:
         start, end, step = args.s.split(':')
@@ -32,18 +36,22 @@ def main():
     else:
         slicer = slice(None)
 
-    if args.mode.lower() == 'rmsd':
+    mode = args.mode.lower()
+
+    if mode == 'rmsd':
         analyzer_class = analysis.RMSD
-    elif args.mode.lower() == 'rmsf':
+    elif mode == 'rmsf':
         analyzer_class = analysis.RMSF
     else:
         raise ValueError('Unrecognized mode, specify one of (RMSD, RMSF)')
 
-    rmsd_aids = cms.select_atom(args.rmsd)
+    rmsd_asl = args.rmsd
+    rmsd_aids = cms.select_atom(rmsd_asl)
     rmsd_gids = topo.aids2gids(cms, rmsd_aids, include_pseudoatoms=False)
     rmsd_ref_pos = trj[int(args.ref)].pos(rmsd_gids)
 
-    fit_aids = cms.select_atom(args.fit)
+    fit_asl = args.fit or rmsd_asl
+    fit_aids = cms.select_atom(fit_asl)
     fit_gids = topo.aids2gids(cms, fit_aids, include_pseudoatoms=False)
     fit_ref_pos = trj[int(args.ref)].pos(fit_gids)
 
@@ -52,17 +60,22 @@ def main():
 
     fh = open(args.o + '.dat', 'x') if args.o else sys.stdout
 
-    for fr, r in zip(trj[slicer], res):
-        fh.write(f"{fr.time} {r}\n")
+    if mode == 'rmsd':
+        for fr, r in zip(trj[slicer], res):
+            fh.write(f"{fr.time} {r}\n")
+    else:
+        for i, r in res:
+            fh.write(f"{i} {r}\n")
 
     if args.p:
-        out = args.o + '.png' if args.o else f'{args.mode}_calc.png'
-        if args.mode.lower() == 'rmsd'
+        out = args.o + '.png' if args.o else f'{mode}_calc.png'
+        if mode == 'rmsd':
             plt.plot([fr.time for fr in trj[slicer]], res)
-        else: # assume RMSD
+            plt.xlabel('time (ps)')
+        else: # assume RMSF
             plt.plot(res)
-        plt.xlabel('time (ps)')
-        plt.ylabel(f'{args.mode} (Å)')
+            plt.xlabel('atom index')
+        plt.ylabel(f'{mode} (Å)')
         plt.savefig(out)
 
 
